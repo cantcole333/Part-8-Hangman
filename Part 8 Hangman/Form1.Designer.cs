28ed94378171112390caf2a59df735399651cd20
Cole Cantwell <CantCole333@GoTVDSB.ca>
2020-10-27 13:28:15 -0400

    Add project files.

---
+++ Part 8 Hangman/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Part_8_Hangman
+{
+    partial class Hangman
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.imgHangman = new System.Windows.Forms.PictureBox();
+            this.lstGuessedLetters = new System.Windows.Forms.ListBox();
+            this.lblUsedLetters = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgHangman)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(228, 248);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(107, 20);
+            this.btnGuess.TabIndex = 0;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // lblWord
+            // 
+            this.lblWord.AutoSize = true;
+            this.lblWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWord.Location = new System.Drawing.Point(12, 271);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(0, 55);
+            this.lblWord.TabIndex = 1;
+            this.lblWord.Click += new System.EventHandler(this.lblWord_Click);
+            // 
+            // imgHangman
+            // 
+            this.imgHangman.Image = global::Part_8_Hangman.Properties.Resources.hangman_empty1;
+            this.imgHangman.Location = new System.Drawing.Point(12, 68);
+            this.imgHangman.Name = "imgHangman";
+            this.imgHangman.Size = new System.Drawing.Size(154, 200);
+            this.imgHangman.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.imgHangman.TabIndex = 3;
+            this.imgHangman.TabStop = false;
+            this.imgHangman.Click += new System.EventHandler(this.imgHangman_Click);
+            // 
+            // lstGuessedLetters
+            // 
+            this.lstGuessedLetters.FormattingEnabled = true;
+            this.lstGuessedLetters.Location = new System.Drawing.Point(191, 89);
+            this.lstGuessedLetters.Name = "lstGuessedLetters";
+            this.lstGuessedLetters.Size = new System.Drawing.Size(110, 134);
+            this.lstGuessedLetters.TabIndex = 4;
+            // 
+            // lblUsedLetters
+            // 
+            this.lblUsedLetters.AutoSize = true;
+            this.lblUsedLetters.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsedLetters.Location = new System.Drawing.Point(187, 62);
+            this.lblUsedLetters.Name = "lblUsedLetters";
+            this.lblUsedLetters.Size = new System.Drawing.Size(114, 24);
+            this.lblUsedLetters.TabIndex = 5;
+            this.lblUsedLetters.Text = "Used Letters";
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(182, 248);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(40, 20);
+            this.txtGuess.TabIndex = 6;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(228, 24);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(35, 13);
+            this.lblStatus.TabIndex = 7;
+            this.lblStatus.Text = "label1";
+            this.lblStatus.Click += new System.EventHandler(this.lblStatus_Click);
+            // 
+            // Hangman
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 335);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.lblUsedLetters);
+            this.Controls.Add(this.lstGuessedLetters);
+            this.Controls.Add(this.imgHangman);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.btnGuess);
+            this.Name = "Hangman";
+            this.Text = "Hangman";
+            this.Load += new System.EventHandler(this.Hangman_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgHangman)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.PictureBox imgHangman;
+        private System.Windows.Forms.ListBox lstGuessedLetters;
+        private System.Windows.Forms.Label lblUsedLetters;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Part 8 Hangman/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part_8_Hangman
+{
+    public partial class Hangman : Form
+    {
+        string word = "COMPUTER";
+        int guess = 0;
+        string displayWord = "_ _ _ _ _ _ _ _";
+        int index;
+        public Hangman()
+        {
+            InitializeComponent();
+        }
+
+        private void lblWord_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            
+            string userGuess = txtGuess.Text.ToUpper();
+            if (word.Contains(userGuess))
+            {
+                index = word.IndexOf(userGuess);
+                displayWord = displayWord.Remove(index*2, 1);
+                displayWord = displayWord.Insert(index*2, userGuess);
+                lblStatus.Text = Convert.ToString(index);
+                lblWord.Text = displayWord;
+                txtGuess.Text = "";
+            }
+            else
+            {
+                lstGuessedLetters.Items.Add(userGuess);
+                guess = guess += 1;
+                if (guess == 1)
+                {
+                    imgHangman.Image = Properties.Resources.hangman_1;
+                }
+                else if (guess == 2)
+                {
+                    imgHangman.Image = Properties.Resources.hangman_2;
+                }
+                else if (guess == 3)
+                {
+                    imgHangman.Image = Properties.Resources.hangman_dead;
+                }
+                if (guess > 3)
+                {
+                    lblStatus.Text = "You lose!";
+                }
+            }
+            
+
+
+        }
+
+        private void Hangman_Load(object sender, EventArgs e)
+        {
+            lblWord.Text = displayWord;
+        }
+
+        private void imgHangman_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
